7b736426bf9880c261309973cc656ad3f6ceb371
Ваня <7328652@gmail.com>
2024-02-08 22:00:23 +0200

    upd routes: signin and profile

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,9 +1,11 @@
 import { Container } from "@mui/material";
+import Link from "next/link";
 
 export default async function Singin() {
   return (
     <Container>
       <h2>SignIn</h2>
+      <Link href="api/auth/signin">Login</Link>
     </Container>
   );
 }
