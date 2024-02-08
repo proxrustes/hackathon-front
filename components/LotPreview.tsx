7aa5d84e324d3f822e7f149689bf0b5dc95cec4a
proxrustes <proxrustes@gmail.com>
2024-02-08 12:51:28 +0100

    lot preview upgrade

--- components/LotPreview.tsx
+++ components/LotPreview.tsx
@@ -7,9 +7,9 @@ export function LotPreview(props: { lot: Lot }) {
             <a style={{ textDecoration: 'none' }} href={`/lot-overview/${props.lot.id}`}>
                 <Paper elevation={3} sx={{ cursor: 'pointer' }}>
                     <Box sx={{ width: "100%", height: 250, backgroundColor: "secondary.main" }} />
-                    <Stack sx={{ p: 1 }}>
-                        <Typography variant="h4">{props.lot.title}</Typography>
-                        <Typography variant="h6">{props.lot.description}</Typography>
+                    <Stack direction="row" justifyContent="space-between" sx={{ p: 1 }}>
+                        <Typography variant="h5" sx={{fontWeight: 800}}>{props.lot.title}</Typography>
+                        <Typography variant="h5">$ {props.lot.price}</Typography>
                     </Stack>
                 </Paper>
             </a>
